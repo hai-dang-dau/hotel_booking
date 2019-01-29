569753853280b8a82335dd1d8225670e9ed43692
Kaerusama <gnudcud@gmai.com>
2019-01-29 19:26:27 +0100

    successfully treated categorical variables

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -7,10 +7,13 @@ class FeatureExtractor:
         pass
     
     def transform(self, X_df):
-        # simple solution: drop all numeric columns
+        # Handle mising data
         X_df_2 = X_df.fillna(-1)
-        non_numeric_columns = ['ArrivalDateMonth', 'Meal', 'Country', 'MarketSegment', 'DistributionChannel', 'ReservedRoomType', 'AssignedRoomType', 'DepositType', 'Agent', 'Company', 'CustomerType']
-        X_df_2 = X_df_2.drop(labels = non_numeric_columns, axis = 1)
-        temp = X_df_2.values
-        assert(str(temp.dtype) != 'object')
-        return temp
+        
+        # Transform categorical features into pandas's categorical type
+        non_numeric_columns = ['ArrivalDateMonth', 'Meal', 'Country', 'MarketSegment', 'DistributionChannel', 
+                               'ReservedRoomType', 'AssignedRoomType', 'DepositType', 'Agent', 'Company', 
+                               'CustomerType']
+        X_df_2[non_numeric_columns] = X_df_2[non_numeric_columns].astype('category')
+        
+        return X_df_2
